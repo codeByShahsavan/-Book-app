a96cd91efba1c9772d396702716ebe3afdcca2bb
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-12-30 19:45:11 +0330

    book app react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Books from "./Components/Books";
+import Layout from "./layouts/Layout";
+
+function App() {
+  return (
+    <>
+    <Layout>
+       <Books/>
+    </Layout>
+    
+    </>
+  );
+}
+
+export default App;
